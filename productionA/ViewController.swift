87e03c9121ff68550c1eea1080c15b237c49cdba
DongYuan <dongyuan@cttq.com>
2015-07-30 15:45:02 +0800

    修改2

--- productionA/ViewController.swift
+++ productionA/ViewController.swift
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
     }
 
     override func didReceiveMemoryWarning() {
+        
+        
+        
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
